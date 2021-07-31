686929a8ce82815e405a491b5ccc3a551cbbd916
ShihabHT <shihabrahman122@gmail.com>
2021-07-31 12:34:48 +0600

    Initial commit

--- draw.py
+++ draw.py
@@ -1,23 +1,32 @@
 import turtle
 
-turtle.Screen().setup(1500, 700)
-turtle.Screen().setworldcoordinates(0, 0, 1500, 700)
+# turtle.Screen().setup(1500, 700)
+# turtle.Screen().setworldcoordinates(0, 0, 1500, 700)
 print("f -- For going 150 units forward")
 print("b -- For going backward maintaining same direction")
 print("r -- For turning 165 degree to the  right and moving 150 units")
 print("l -- For turning 165 degree to the left and moving 150 units")
 
+t = turtle.Turtle()
+turtle.Screen().setup(1000, 700)
+# turtle.tracer()
+
 while True:
     control = input().upper()
     if control == 'F':
-        turtle.forward(150)
+        t.forward(150)
     elif control == 'B':
-        turtle.backward(150)
+        t.backward(150)
     elif control == 'R':
-        turtle.right(165)
-        turtle.forward(150)
+        t.right(165)
+        t.forward(150)
     elif control == 'L':
-        turtle.left(165)
-        turtle.forward(150)
+        t.left(165)
+        t.forward(150)
+    elif control == 'X':
+        break
     else:
         pass
+turtle.update()
+turtle.mainloop()
+
